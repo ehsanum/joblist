ebceca46c93f79646ec93893ea61e53cf66c9651
Clever Antonio <98922737+ehsanum@users.noreply.github.com>
2023-12-20 08:23:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,316 @@
+const jobs = [
+    {
+      title: "Electrician || PG-timbers",
+      image: "images/software-engineer.svg",
+      details:
+       "HND/Degree in Electrical Engineering or equivalent.Familiarity with Electricity (Public Safety) Regulation, 2018; is an added advantage, preferably with work experience in electrical machinery, maintenance and installation a high speed, high volume manufacturing environment (Timber processing industry)",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/electrician-pg-timbers/",
+    },
+  
+  
+    {
+      title: "Workshop Foreman || Toyota Zimbabwe",
+      image: "images/marketing-manager.svg",
+      details:
+       "Class One Journeyman in Automotive Industry and/or Professional qualifications in a automotive engineering .Successful hands-on experience in a Service environment at a supervisory level for at least 2 years (can be flexible depending on ability and potential) and at least a Certificate in Supervisory Management.",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/workshop-foreman-toyota-zimbabwe/",
+    },
+  
+  
+    {
+      title: "Medical Laboratory Technician X 4 Posts || Lancet Clinical Laboratories",
+      image: "images/marketing-manager.svg",
+      details:
+       "Performs diagnostic testing procedures on instrumentation, analyses results in one or more sections of a laboratory and interacts with pathologists on technical matters to provide all relevant data that may pertain to the prevention, diagnosis or treatment of diseases Records, verifies and amend (when required) test results to ensure integrity of test result database on laboratory information system.",
+      openPositions: "4",
+      link: "https://vacancybox.co.zw/job/medical-laboratory-technician-x-4-posts-lancet-clinical-laboratories/",
+     },
+  
+  
+   {
+      title: "Freelance Sales Rep || Veer Freight Private Limited",
+      image: "images/marketing-manager.svg",
+      details:
+       "• 5 O levels • Be at least 18- 35 years of age • Must have a certificate or Diploma in sales and • Marketing • Knowledge of the security industry an added advantage • Proven work experience as a Sales Representative • Excellent knowledge of MS Office, PowerPoint and excel • Highly motivated and target driven with a proven track record in sales • Excellent selling, negotiation and communication skills",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/freelance-sales-rep-veer-freight-private-limited/",
+       },
+  
+  
+    {
+      title: "Civil Works Foreman || Green Fuel",
+      image: "images/marketing-manager.svg",
+      details:
+       "Full installation of Property Development Infrastructure. Roads, Sewer, Water, Electricity Lines, Storm Water Drainage, Culverts, Head Walls etc || Be able to manage large civil construction environments. Multiple sites at once || Be able to Work Under Pressure and Timelines || Day to day management of site activities. Plant and operator management.",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/civil-works-foreman-green-fuel/",
+    },
+  
+  
+    {
+      title: "Parts Manager || Croco Motors",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 21 DEC 2023， Applications are invited from interested and suitably qualified persons to fill in the vacancy that has arisen within the organization.",   
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/parts-manager-croco-motors/",
+    },
+  
+  
+    {
+      title: "Account Relationship Officer || BancABC Zimbabwe",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 20 DEC 2023 ，Account Relationship Officer (Agribusiness Chiredzi Branch)||Job Description ： Catering for all service issues and ensuring customer satisfaction, maximizing sales, credit applications, service, and profit performance in the Agribusiness Portfolio and Branch. To develop mutually beneficial long-term relationships with the bank’s Retail Agribusiness customers.",
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/account-relationship-officer-bancabc-zimbabwe/",
+    },
+  
+  
+   {
+      title: "Procurement Assistant​​​​​​​ || Organic Africa",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 31 DEC 2023,The role will be responsible for ensuring timely, cost-effective procurement and delivery of goods and services, which are compliant with the company’s procurement policies and guidelines. ",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/procurement-assistant-organic-africa/",
+    },
+  
+  
+    {
+      title: "Content Creator || Azbo Investments",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 19 DEC 2023,Job Description Azbo Investments is looking for a driven, detail-oriented Content Creator to produce high-quality outputs for our business. The Content Creator will create material, and identify new ways to reach consumers. Content Creator will promote collaboration between internal stakeholders to ensure consistency in our company’s outputs.",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/content-creator-azbo-investments/",
+    },
+  
+  
+    {
+      title: "Internal Auditor || Zimstat",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 03 DEC 2023,Job Purpose: The job holder will be responsible for assisting the Agency to accomplish its objectives by carrying out audits in a systematic and disciplined approach by evaluating the effectiveness of risk management, internal controls and governance issues.",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/internal-audit-zimstat/",
+    },
+  
+  
+    {
+      title: "Assistant Accountant || Galaway Management Services",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 18 DEC 2023, Applications are invited from suitably qualified, experienced, self-motivated and task-oriented individuals to fill the position of ASSISTANT ACCOUNTANT",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/assistant-accountant-galaway-management-services/",
+    },
+  
+  
+    {
+      title: "Loss Control Officer || Galaway Management Services",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 18 DEC 2023 , Applications are invited from suitably qualified, experienced, self-motivated and task-oriented individuals to fill the position of LOSS CONTROL OFFICER. || LOCATION BULAWAYO",   
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/loss-control-officer-galaway-management-services-3/",
+    },
+  
+  
+    {
+      title: "Insurance Consultants || AFC Insurance",
+      image: "images/marketing-manager.svg",
+      details:
+       "The Insurance Consultant positions are in the following Branches: Harare, Mutare, Bulawayo, Masvingo, Chegutu, Chinhoyi, Gwanda, Bindura, Marondera, Rusape, Gweru, Hwange, Maphisa, Zvishavane, Chiredzi and Nyika.",   
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/insurance-consultants-afc-insurance/",
+    },
+  
+  
+    {
+      title: "Safety, Health, Environment and Quality (SHEQ) Officer || Zimbabwe School of Mines",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 31 DEC 2023 ,Job Summary: The position exists to execute the school’s mandate as stipulated in the ZSM Charter, coordinating, developing, evaluating, and executing Zimbabwe School of Mines policies and procedures that will ensure the health and safety of faculty, staff, students, and visitors; environmental compliance and protection; and quality assurance of the School’s SHEQ management system as directed by the principal",   
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/safety-health-environment-and-quality-sheq-officer-zimbabwe-school-of-mines/",
+    },
+  
+  
+    {
+      title: "Shop Manager || First Pack Marketing",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 29 DEC 2023 , The Shop Manager is responsible for ensuring that the branch delivers its objectives. This includes delivering good customer service, branch performance, cash controls, stock accountability, staff management and maintenance of branch ‘facilities. The Shop Manager is responsible for ensuring that the store offers clients first class service.",    
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/shop-manager-first-pack-marketing/",
+    },
+  
+    {
+      title: "Deputy Head || Quinton Junior School",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 24 DEC 2023, TITLE: Deputy Head-Junior School \ LOCATION: Chitungwiza. In its quest to maintain astronomical educational standards, Quinton Junior School is seeking a remarkable and avid individual to join its team as the Deputy Head. As the Deputy Head, you will play a vital role in the realization of the school’s Vision through providing strong leadership and support to the Quinton school community.",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/deputy-head-quinton-junior-school/",
+     },
+     
+    {
+      title: "Refuelling Assistant – Masvingo || Econet Wireless",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 30 NOV 2023 :Job Purpose:  To refuel all generators within set key performance indicators so as to ensure set network uptime. The incumbent will also top up ZESA recharge units at all base stations so as to ensure set network uptime  ",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/refuelling-assistant-masvingo-econet-wireless/",
+    },
+  
+  
+    {
+      title: "Project Accountant || Zim-TTECH",
+      image: "images/marketing-manager.svg",
+      details:
+       "Deadline:18 December ,We are seeking highly qualified and experienced applicants to be considered for the position of Project Accountant for the HIV care and treatment programs. Zim-TTECH’s activities occur primarily in the technical areas of health system strengthening; epidemiology and strategic information strengthening, health workforce development; operations research and evaluation; prevention, care, and treatment of infectious diseases.",   
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/project-accountant-zim-ttech/",
+    },
+  
+  
+    {
+      title: "Loan Officers || Wildfin Financial Services",
+      image: "images/marketing-manager.svg",
+      details:
+       "Wildfin Financial Services is hereby looking for energetic young people to be trained as loans officers with at least A level good passes.Passing of commercial subjects is an added advantage.",   
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/loan-officers-wildfin-financial-services-3/",
+    },
+  
+  
+    {
+      title: "Assistant / Senior Assistant Librarian || Great Zimbabwe University",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 22 DEC 2023, ASSISTANT/SENIOR ASSISTANT LIBRARIAN – RESEARCH DISCOVERY AND DISSEMINATION | LIBRARY Job Details: QUALIFICATIONS ; Applicants must have at least a first degree in Library and Information Sciences or equivalent plus four (4) years post-qualification experience or a Master’s degree in Library and Information Sciences or equivalent plus two (2) years post-qualification experience. Practical knowledge of digital library systems would be an added advantage. In addition, applicants must have five (5) Ordinary level passes, including the English Language.",    
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/assistant-senior-assistant-librarian-great-zimbabwe-university/",
+    },
+  
+  
+    {
+      title: "Seargent: Security Services X 2 Posts || Great Zimbabwe University",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 22 DEC 2023, Job Details: Applicants should have a security qualification, five (5) years  continuous work experience in the Security industry and five (5) Ordinary level passes including English Language. In addition, applicants must be able to work under pressure and meet deadlines.",
+       openPositions: "2",
+      link: "https://vacancybox.co.zw/job/seargent-security-services-great-zimbabwe-university/",
+      },
+  
+  
+    {
+      title: "Stock Controller || Galaway Management Services",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 13 DEC 2023 , Applications are invited from suitably qualified and experienced candidates to fill the below mentioned position which has arisen in a fast-growing retail company.",
+       openPositions: "1",
+      link: "https://vacancybox.co.zw/job/stock-controller-galaway-management-services/",
+    },
+  
+  
+    {
+      title: "Consultancy for Upgrading and Restoration of the DREAMS-RISE Program DHIS2 System: DATIS || Zimbabwe Health Interventions (ZHI)",
+      image: "images/marketing-manager.svg",
+      details:
+       "Diagnose and troubleshoot the already existing DATIS information management system, innovate and offer lasting solutions, 2. Revitalize the infrastructure of the DATIS database, 3. Develop and configure all custom indicators 4. Design and develop an android based mobile interface capable of offline data capture, 5. Create an interoperable system that automatically transfers data between",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/consultancy-for-upgrading-and-restoration-of-the-dreams-rise-program-dhis2-system-datis-zimbabwe-health-interventions-zhi/",
+      },
+  
+  
+    {
+      title: "SME Hosting Support Officer || Webdev",
+      image: "images/marketing-manager.svg",
+      details:
+       " DUE: 29 DEC 2023 : SME HOSTING SUPPORT OFFICER VACANCY || Applications are invited from suitably qualified candidates for the above vacant position. Webdev (Pvt) Ltd is Zimbabwe’s leading web & e-mail hosting, online marketing, e-commerce, online payments and web development company, a multi-award-winning company and also a market leader. Webdev is an employer of equal opportunity and offers a competitive salary and benefits.",
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/sme-hosting-support-officer-webdev/",
+    },
+  
+    {
+      title: "Marketing Officer || Webdev",
+      image: "images/marketing-manager.svg",
+      details:
+       "DUE: 29 DEC 2023, MARKETING OFFICER VACANCY , Applications are invited from suitably qualified candidates for the above vacant position. Webdev (Pvt) Ltd is Zimbabwe’s leading web & e-mail hosting, online marketing, e-commerce, online payments and web development company, a multi-award-winning company and also a market leader. Webdev is an employer of equal opportunity and offers a competitive salary and benefits.",   
+      openPositions: "1",
+      link: "https://vacancybox.co.zw/job/marketing-officer-webdev/",
+    },
+  
+  
+  ];
+  
+  const jobsHeading = document.querySelector(".jobs-list-container h2");
+  const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+  const jobSearch = document.querySelector(".jobs-list-container .job-search");
+  
+  
+  let searchTerm = "";
+  if (jobs.length == 1) {
+    jobsHeading.innerHTML = `${jobs.length} Job`;
+  } else {
+    jobsHeading.innerHTML = `${jobs.length} Jobs`;
+  }
+  
+  
+  const createJobListingCards = () => {
+    jobsContainer.innerHTML = "";
+    jobs.forEach((job) => {
+      if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+        let jobCard = document.createElement("div");
+        jobCard.classList.add("job");
+  
+        let image = document.createElement("img");
+        image.src = job.image;
+  
+        let title = document.createElement("h3");
+        title.innerHTML = job.title;
+  
+        title.classList.add("job-title");
+        let details = document.createElement("div");
+  
+        details.innerHTML = job.details;
+        details.classList.add("details");
+  
+  
+        let detailsBtn = document.createElement("a");
+        detailsBtn.href = job.link;
+        detailsBtn.innerHTML = "More Details";
+  
+        detailsBtn.classList.add("details-btn");
+        let openPositions = document.createElement("span");
+        openPositions.classList.add("open-positions");
+        if (job.openPositions == 1) {
+          openPositions.innerHTML = `${job.openPositions} open position`;
+        } else {
+          openPositions.innerHTML = `${job.openPositions} open positions`;
+        }
+  
+  
+        jobCard.appendChild(image);
+        jobCard.appendChild(title);
+        jobCard.appendChild(details);
+        jobCard.appendChild(detailsBtn);
+        jobCard.appendChild(openPositions);
+        jobsContainer.appendChild(jobCard);
+  
+      }
+  
+    });
+  
+  };
+  
+  
+  createJobListingCards();
+  jobSearch.addEventListener("input", (e) => {
+    searchTerm = e.target.value;
+    createJobListingCards();
+  });
